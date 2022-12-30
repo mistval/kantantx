dcc789cd3e018105d8e532f5f6117b0dbd9f023e
mistval <randallschmidt1@gmail.com>
2022-12-30 15:46:12 -0500

    stuff

---
+++ server/src/types/api_schemas/documents.ts
@@ -0,0 +1,11 @@
+import Joi from "joi";
+
+export interface IUpdateDocumentRequest {
+  operation: 'move';
+  newName: string;
+}
+
+export const updateDocumentRequestSchema = Joi.object<IUpdateDocumentRequest>({
+  operation: Joi.valid('move').required(),
+  newName: Joi.string().min(1).required(),
+});
